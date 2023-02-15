4ffde8aa2aec3bdd62ad078720157ad49c4e8400
Caleb Taylor <aquaductape@gmail.com>
2023-02-15 12:42:44 -0800

    fix unwanted scrollbar shift animation on settings panel

--- src/utils/hasScrollbar.ts
+++ src/utils/hasScrollbar.ts
@@ -2,5 +2,5 @@ export const hasScrollbarY = (el: HTMLElement) => {
   const { offsetWidth, clientWidth } = el;
   if (clientWidth === 0) return false;
 
-  return clientWidth === offsetWidth;
+  return clientWidth !== offsetWidth;
 };
